b621ff93f90539408951bfdc8e5370688beea813
BGTUwork <ngtaranina@gmail.com>
2025-01-10 18:36:54 +0300

    Create greeting.py file

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Nataliya'
+title = 'first note'
+content = 'my new note'
+status = 'begin'
+created_date = '10.01.2025'
+issue_date = '10.02.2025'
+print(f'имя пользователя: {username}')
+print(f'название заметки: {title}')
+print(f'содержание заметки: {content}')
+print(f'статус заметки: {status}')
+print(f'дата создания заметки: {created_date}')
+print(f'дата дедлайна: {issue_date}')
