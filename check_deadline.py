3fc349fbeb6ae29c31eac7026772f869d5a78261
ngtaranina <ngtaranina@hotmail.com>
2025-01-23 16:58:02 +0300

    fix: 23/01/2025 changed check_format_data ()

--- check_deadline.py
+++ check_deadline.py
@@ -6,20 +6,13 @@ issue_date = '' # переменная для хранения даты дедл
 
 #  функция проверки правильности формата ввода даты
 def check_format_data (dat: str):
-    global issue_date
     if len(dat) > 10:
-        print('ошибка в формате ввода даты')
+        print('что-то очень длинная дата получилась')
         return False
-    if '.' in dat:
-        issue_date = dat.split('.')
-        print(dat)
-        return True
-    elif '/' in dat:
-        issue_date = dat.split('/')
-        print(dat)
+    if '.' in dat or '/' in dat:
         return True
     else:
-        print('ошибка в формате ввода даты')
+        print('что-то не так в формате ввода даты')
         return False
 
 # функция, которая будет проверять, что такая дата существует
@@ -30,12 +23,18 @@ def check_exist_data(y, m, d):
     except:
         return False
 
+# основной блок программы
 while main_condition:
     issue_date = input('Введи дату дедлайна в формате dd.mm.yyyy или dd/mm/yyyy:  ')
     format_condition = check_format_data (issue_date)
     if format_condition:
         # Проверка на формат выполнена, создаем объект issue_date, как элемент datetime.datetime
         # сначала выделяем значения дня, месяца и года
+        if '.' in issue_date:
+            issue_date = issue_date.split('.')
+        elif '/' in issue_date:
+            issue_date = issue_date.split('/')
+        # преобразовываем их в формат целых чисел
         us_date = int(issue_date[0])
         us_month = int(issue_date[1])
         us_year = int(issue_date[2])
@@ -45,15 +44,12 @@ while main_condition:
             issue_date = datetime.datetime(us_year, us_month, us_date)
             main_condition = False
         else:
-            print('Ошибка в дате. Такой даты не существует')
-
+            print('Упс! Такой даты не существует!')
 
 # определяем разницу между сегодняшней датой и датой дедлайна
 delta = (issue_date - today).days + 1
-print(delta)
-
 if delta > 0:
-    print(f'до дедлайна осталось {delta} дня')
+    print(f'до дедлайна осталось {delta} дн(я/ей)')
 elif delta == 0:
     print('Дедлайн сегодня! Торопись!')
 else:
